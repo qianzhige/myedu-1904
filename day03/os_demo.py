fdad91fbd18b70be4edb5cb9e350e8e141388674
王沨 <wf06137452@163.com>
2019-05-31 19:56:48 +0800

    5.31

---
+++ day03/os_demo.py
@@ -0,0 +1,14 @@
+import os
+# os 是操作目录和权限的
+if __name__ == '__main__':
+    # os.getcwd()  获取当前目录/路径
+    pwd = os.getcwd()
+    print(pwd)
+
+    # 获取上级目录的字符串
+    a = os.path.abspath('..')
+    print(a)
+
+    # 获取上级目录的字符串
+    b = os.path.abspath('../..')
+    print(b)
---
+++ day03/while_demo.py
@@ -0,0 +1,9 @@
+
+def while_demo():
+    a = 0
+    while a<5:    # while 条件 : 下面写要循环的代码 (如果 while Ture:  则不停止)
+        print(a)
+        a+=1
+
+if __name__ == '__main__':
+    while_demo()
